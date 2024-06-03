9e52dea452f67472bcfdcbe14ce3f3984466ee6d
Jacob Eggerd <jfeggerd@gmail.com>
2024-06-03 14:44:48 -0400

    Trying adversarial loss

--- ssim.py
+++ ssim.py
@@ -8,35 +8,9 @@ class SSIM(nn.Module):
 
 
     def forward(self, raw_tensor: Tensor, dst_tensor: Tensor) -> Tensor:
-
-        print(raw_tensor.shape[0])
-
-        # for i in range(raw_tensor.shape[0]):
-        #     sr = raw_tensor[i, :, :, :]
-        #     gt = dst_tensor[i, :, :, :]
-
-        #     print(sr.shape)
-
-        #     ssim_val = ssim(sr, gt, data_range=1)
-        #     print(ssim_val)
-        
-        # exit()
         
         ssim_val = ssim(raw_tensor, dst_tensor, data_range=1, size_average=False, nonnegative_ssim=True)
 
-        # for i in range(raw_tensor.shape[0]):
-        #     for j in range(96):
-        #         for k in range(96):
-        #             print(raw_tensor[i, 0, j, k].item())
-
-        # print(ssim_val)
-        # print(ssim_val.size())
-
         ssim_val = torch.sub(1, ssim_val)
-
-        # print(ssim_val)
-        # print(ssim_val.size())
-
-        # exit()
         
         return ssim_val

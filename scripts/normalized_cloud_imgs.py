d3810255e5c07045f8befa1c8297dc17db44057e
Jacob Eggerd <jfeggerd@gmail.com>
2024-06-05 15:53:20 -0400

    New configs for new dataset

--- scripts/normalized_cloud_imgs.py
+++ scripts/normalized_cloud_imgs.py
@@ -12,6 +12,8 @@ x_lim = 28000
 y_lim = 28000
 count = 0
 
+imgs = []
+
 while count < 100:
     z = random.randint(300,1000)
     x = random.randint(1, (x_lim / 2000)) * 2000
@@ -21,14 +23,30 @@ while count < 100:
     img = vol[x-1000:x, y-1000:y, z]
     img = img[:, :, 0, 0]
 
-    if np.std(img) < 17 or np.max(img) < 400:
+    if np.std(img) < 17:
         continue
 
     count += 1
 
+    imgs.append(img)
+
     print(count) 
 
-    img = img.astype(np.float16) / 500
+percentiles = []
+for i in range(len(imgs)):
+    percentiles.append(np.percentile(imgs[i], 99.99))
+
+percentiles = np.array(percentiles)
+
+percentile = percentiles.mean()
+
+print(percentile)
+
+for i in range(len(imgs)):
+
+    img = imgs[i]
+
+    img = img.astype(np.float16) / 360
 
     img[img > 1] = 1
 
@@ -36,11 +54,11 @@ while count < 100:
 
     img = (img).astype('uint8')
 
-    cv2.imwrite("./data/real_hr_test_normal/test_" + str(x) + "_" + str(y) + "_" + str(z) + ".png", img)
+    cv2.imwrite("./data/real_hr_train_normal/test_" + str(i) + ".png", img)
 
     img_lr = cv2.resize(img, (int(img.shape[0] / 4), int(img.shape[1] / 4)))
 
-    cv2.imwrite("./data/real_lr_test_normal/test_" + str(x) + "_" + str(y) + "_" + str(z) +".png", img_lr)
+    cv2.imwrite("./data/real_lr_train_normal/test_" + str(i) +".png", img_lr)
 
 
 
